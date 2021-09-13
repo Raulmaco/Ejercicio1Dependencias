a788741e12c5dd6861ae5d12a45da8d65d84ccf9
Raúl Martínez <raul.martinez@bosonit.com>
2021-09-13 10:01:08 +0200

    Initial commit

---
+++ src/main/java/com/example/Ejercicio1Dependencias/Ejercicio1DependenciasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ejercicio1Dependencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio1DependenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio1DependenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ejercicio1Dependencias/Ejercicio1DependenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ejercicio1Dependencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio1DependenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
